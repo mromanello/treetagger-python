1bec27a57d567a6f66c4c290936aebf9af790f83
Matteo Romanello <matteo.romanello@gmail.com>
2017-03-14 17:06:32 +0100

    tweaked the setup.py script to be more py2-friendly

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ try:
 except ImportError:
     from distutils.core import setup
 
+module_name = "treetagger"
+if(sys.version_info < (3, 0)):
+  module_name = "treetagger_python2"
+  
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
@@ -17,7 +21,7 @@ setup(name='treetagger',
       author='Mirko Otto',
       author_email='dropsy@gmail.com',
       url='https://github.com/miotto/treetagger-python',
-      py_modules=['treetagger'],
+      py_modules=[module_name],
       install_requires=['nltk'],
       license='GPL Version 3',
     )
